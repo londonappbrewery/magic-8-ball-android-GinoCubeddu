3a1d6f009ddcc8711828083da7a59fe1b6835ae9
GinoCubeddu <CUBG1_14@uni.worc.ac.uk>
2018-01-31 14:35:54 +0000

    Create code that allows the magic eight ball to function

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,13 +1,45 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Random object to choose which ball to choose
+    private Random rnd;
+
+    // Image view to show the ball
+    private ImageView ballImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate variables
+        rnd = new Random();
+        ballImageView = findViewById(R.id.image_eightBall);
+
+        final int[] images = {
+            R.drawable.ball1,
+            R.drawable.ball2,
+            R.drawable.ball3,
+            R.drawable.ball4,
+            R.drawable.ball5
+        };
+
+        // Create onclick for the button
+        findViewById(R.id.askButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ballImageView.setImageResource(images[rnd.nextInt(images.length)]);
+            }
+        });
     }
 }
